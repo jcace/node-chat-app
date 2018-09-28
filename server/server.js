7cf90204330f840997e9342ff11a94a31446bb55
Jason Cihelka <jason.cihelka@jcace.com>
2018-09-28 12:04:00 -0600

    Add socket

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '/../public');
 const express = require('express');
-const app = express();
+const socketIO = require('socket.io');
+const http = require('http');
 const port = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+// Pass in server as argument to socket. io is the web sockets server.
+var io = socketIO(server);
+
+// Event listener
+io.on('connection', (socket) => {
+  console.log("New user connected");
+
+  socket.on('disconnect', () => {
+    console.log("user disconnected");
+  })
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`App listening on ${port}!`));
+server.listen(port, () => console.log(`App listening on ${port}!`));
 
